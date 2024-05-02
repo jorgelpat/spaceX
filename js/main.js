1e74f7a8087e88a9be4bd4074758aa18949c897a
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-05-02 13:23:41 -0500

    feat: :sparkles: se agregan elementos de medicion a modulo en html

--- js/main.js
+++ js/main.js
@@ -1,27 +1,53 @@
-import {getAllRockets} from "./modules/rockets.js"
+import { getAllRockets } from "./modules/rockets.js";
+import { rocketGeneralInformation } from "./modulesComponents/box.js";
+import { 
+    progressRocketWeight, 
+    progressPayloadWeights, 
+    progressHeightRocket, 
+    progressDiameterRocket,
+    progressSecondStageDiameterRocket,
+    progressSecondStageHeightRocket
+} from "./modulesComponents/progressBar.js";
 
-let information__2 = document.querySelector("#information__2");
-let dataRockets = await getAllRockets();
-console.log(dataRockets);
-dataRockets.forEach(val => {
-    let divInformationContainer = document.createElement("div");
-    divInformationContainer.classList.add("information__container");
-    let divFirst = document.createElement("div");
-    let labelFist = document.createElement("label");
-    labelFist.textContent = "Mass";
-    let ProgressFist = document.createElement("progress");
-    ProgressFist.max = 100;
-    ProgressFist.value = `${val.mass.kg / 1000}`;
-    ProgressFist.textContent = `${val.mass.kg / 1000}%`;
-    let divLast = document.createElement("div");
-    let spanLast = document.createElement("span");
-    spanLast.textContent = "Lorem ipsum..";
+document.addEventListener("DOMContentLoaded", async () => {
+    let information__2 = document.querySelector("#information__2");
+    let information__1 = document.querySelector("#description__item");
 
-    divFirst.append(labelFist);
-    divFirst.append(ProgressFist)
-    divLast.append(spanLast)
-    divInformationContainer.append(divFirst)
-    divInformationContainer.append(divLast)
-    console.log(divInformationContainer);
-    information__2.append(divInformationContainer);
+    let dataRockets = await getAllRockets();
+    let Totales = dataRockets.pop();
+    let [Rocket1, Rocket2, Rocket3, Rocket4] = dataRockets;
+
+    console.log(dataRockets);
+    console.log(Totales);
+
+    // information__2.append(...progressRocketWeight(Totales.kg_max, Rocket1));
+    // information__2.append(...progressPayloadWeights(Totales.payload_weights, Rocket1));
+    // information__2.append(...progressHeightRocket(Totales.height, Rocket1));
+    // information__2.append(...progressDiameterRocket(Totales.diameter, Rocket1));
+    // information__2.append(...progressSecondStageDiameterRocket(Totales.composite_diameter, Rocket1));
+    // information__2.append(...progressSecondStageHeightRocket(Totales.composite_height, Rocket1));
+
+    // information__2.append(...progressRocketWeight(Totales.kg_max, Rocket2));
+    // information__2.append(...progressPayloadWeights(Totales.payload_weights, Rocket2));
+    // information__2.append(...progressHeightRocket(Totales.height, Rocket2));
+    // information__2.append(...progressDiameterRocket(Totales.diameter, Rocket4));
+    // information__2.append(...progressSecondStageDiameterRocket(Totales.composite_diameter, Rocket2));
+    // information__2.append(...progressSecondStageHeightRocket(Totales.composite_height, Rocket2));
+
+    information__2.append(...progressRocketWeight(Totales.kg_max, Rocket3));
+    information__2.append(...progressPayloadWeights(Totales.payload_weights, Rocket3));
+    information__2.append(...progressHeightRocket(Totales.height, Rocket3));
+    information__2.append(...progressDiameterRocket(Totales.diameter, Rocket3));
+    information__2.append(...progressSecondStageDiameterRocket(Totales.composite_diameter, Rocket3));
+    information__2.append(...progressSecondStageHeightRocket(Totales.composite_height, Rocket3));
+
+    // information__2.append(...progressRocketWeight(Totales.kg_max, Rocket4));
+    // information__2.append(...progressPayloadWeights(Totales.payload_weights, Rocket4));
+    // information__2.append(...progressHeightRocket(Totales.height, Rocket4));
+    // information__2.append(...progressDiameterRocket(Totales.diameter, Rocket4));
+    // information__2.append(...progressSecondStageDiameterRocket(Totales.composite_diameter, Rocket4));
+    // information__2.append(...progressSecondStageHeightRocket(Totales.composite_height, Rocket4));
+
+    // information__3.innerHTML = rocketGeneralInformation([Rocket2]); // Pasamos un array con un solo cohete
+    information__1.innerHTML = rocketGeneralInformation([Rocket1]); // Pasamos un array con un solo cohete
 });
