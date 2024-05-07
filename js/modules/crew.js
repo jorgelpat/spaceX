bc2d3786a432628b5ba6fae0367b5799232e9477
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-05-07 17:56:45 -0500

    style: :alembic: Se repasa uno a uno el código

--- js/modules/crew.js
+++ js/modules/crew.js
@@ -15,15 +15,28 @@
 
 // getMeh()
 
-const endpoint = "https://api.spacexdata.com/v4/crew";
+// const endpoint = "https://api.spacexdata.com/v4/crew";
 
-const getMeh = async () => {
-    const body = document.querySelector('body');
+// const getMeh = async () => {
+//     const body = document.querySelector('body');
   
-    const response = await fetch(endpoint);
-    const crewMembers = await response.json();
+//     const response = await fetch(endpoint);
+//     const crewMembers = await response.json();
   
-    body.innerHTML = crewMembers.map(member => `<img src="${member.image}" referrerpolicy="no-referrer">`).join('');
+//     body.innerHTML = crewMembers.map(member => `<img src="${member.image}" referrerpolicy="no-referrer">`).join('');
+// }
+
+// getMeh();
+
+const endpoint = "https://api.spacexdata.com/v4/crew";
+const getMeh = async()=>{
+    const body = document?.querySelector("body");   // se pone document?. para acceso seguro
+    let htmlContent = "";
+    const clients = await (await fetch(`${endpoint}`)).json();
+    clients.forEach(element => {
+        htmlContent += `<img src="${element.image}" referrerpolicy="no-referrer"`;
+    });
+    body.innerHTML = htmlContent;
 }
 
 getMeh();
